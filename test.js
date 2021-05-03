67cb3544702d25d0a18820cdb9b039c1bb4cee1e
a-gomathi <65897231+a-gomathi@users.noreply.github.com>
2021-05-03 13:03:43 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 console.log("process.env.Docker username \t: " + process.env.Docker_USR);
 console.log("process.env.Docker password \t: " + process.env.Docker_PSW);
+
+
+const options = {
+      method: 'GET',
+      uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:urbancode/ucv-ext-jira:pull`,
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(process.env.Docker_USR + ':' + process.env.Docker_PSW).toString('base64')
+      }
+    }
+console.log("\n options with base64\t: " + JSON.stringify(options));
